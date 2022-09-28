166057957788d073472b35ccb31478d242c70f93
Tamas Szucs <tamas@originbyte.io>
2022-09-28 11:08:45 +0200

    add libsodium thirdparty source

--- Plugins/LibsodiumUE/Source/ThirdParty/LibsodiumUELibrary/LibsodiumUELibrary.Build.cs
+++ Plugins/LibsodiumUE/Source/ThirdParty/LibsodiumUELibrary/LibsodiumUELibrary.Build.cs
@@ -12,25 +12,7 @@ public class LibsodiumUELibrary : ModuleRules
 		if (Target.Platform == UnrealTargetPlatform.Win64)
 		{
 			// Add the import library
-			PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "x64", "Release", "ExampleLibrary.lib"));
-
-			// Delay-load the DLL, so we can load it from the right place first
-			PublicDelayLoadDLLs.Add("ExampleLibrary.dll");
-
-			// Ensure that the DLL is staged along with the executable
-			RuntimeDependencies.Add("$(PluginDir)/Binaries/ThirdParty/LibsodiumUELibrary/Win64/ExampleLibrary.dll");
-        }
-        else if (Target.Platform == UnrealTargetPlatform.Mac)
-        {
-            PublicDelayLoadDLLs.Add(Path.Combine(ModuleDirectory, "Mac", "Release", "libExampleLibrary.dylib"));
-            RuntimeDependencies.Add("$(PluginDir)/Source/ThirdParty/LibsodiumUELibrary/Mac/Release/libExampleLibrary.dylib");
-        }
-        else if (Target.Platform == UnrealTargetPlatform.Linux)
-		{
-			string ExampleSoPath = Path.Combine("$(PluginDir)", "Binaries", "ThirdParty", "LibsodiumUELibrary", "Linux", "x86_64-unknown-linux-gnu", "libExampleLibrary.so");
-			PublicAdditionalLibraries.Add(ExampleSoPath);
-			PublicDelayLoadDLLs.Add(ExampleSoPath);
-			RuntimeDependencies.Add(ExampleSoPath);
-		}
+			PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "Build", "Release", "x64", "libsodium.lib"));
+        }     
 	}
 }
