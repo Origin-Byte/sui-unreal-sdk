61dca600dfacb207e1ca4392da9829d692b862aa
Tamas Szucs <tamas@originbyte.io>
2022-09-30 16:24:54 +0200

    UInt32ToFString

--- Plugins/SuiUnrealSDKCore/Source/SuiUnrealSDKCore/Public/Util.h
+++ Plugins/SuiUnrealSDKCore/Source/SuiUnrealSDKCore/Public/Util.h
@@ -14,4 +14,8 @@ public:
     {
         return FString::Printf(TEXT("%llu"), Value);
     }
+	static FString UInt32ToFString(uint32 Value)
+    {
+    	return FString::Printf(TEXT("%u"), Value);
+    }
 };
