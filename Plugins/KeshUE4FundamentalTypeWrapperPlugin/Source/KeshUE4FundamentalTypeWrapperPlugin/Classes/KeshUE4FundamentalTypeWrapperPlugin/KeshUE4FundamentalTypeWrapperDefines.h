c6f3f6f50642b02b61ba42120ad63a4a3a8b6543
Tamas Szucs <tamas@originbyte.io>
2022-10-02 22:48:31 +0200

    removed type wrapper plugin

--- Plugins/KeshUE4FundamentalTypeWrapperPlugin/Source/KeshUE4FundamentalTypeWrapperPlugin/Classes/KeshUE4FundamentalTypeWrapperPlugin/KeshUE4FundamentalTypeWrapperDefines.h
+++
@@ -1,8 +0,0 @@
-// Copyright 2014-2015 Matthew Chapman, Inc. All Rights Reserved.
-
-#pragma once
-
-#undef NULL
-#define NULL nullptr
-#define KCPPPWPLog( Level, Text ) UE_LOG( LogKeshUE4FundamentalTypeWrapperPlugin, Level, TEXT( Text ) )
-#define KCPPPWPLogF( Level, Format, ... ) UE_LOG( LogKeshUE4FundamentalTypeWrapperPlugin, Level, TEXT( Format ), __VA_ARGS__ )
